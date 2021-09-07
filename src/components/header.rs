c818df91d3e03cdc4d152f1eb1d0c17679d7f114
Yesterday17 <mmf@mmf.moe>
2021-09-07 23:18:53 +0800

    Link route for header

--- src/components/header.rs
+++ src/components/header.rs
@@ -1,10 +1,14 @@
+use crate::router::Route;
 use yew::prelude::*;
+use yew_router::prelude::Link;
 
 #[function_component(Header)]
 pub fn header() -> Html {
     html! {
         <header class="bg-gray-800 p-4">
-            <h1 class="text-xl font-bold text-white">{ "Annil-Web" }</h1>
+            <Link<Route> route={Route::Index}>
+                <h1 class="text-xl font-bold text-white">{ "Annil-Web" }</h1>
+            </Link<Route>>
         </header>
     }
 }
